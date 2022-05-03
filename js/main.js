f0ae8d2a464804d76f0ad5d393617bf5830413b0
Alexei1982 <80078429+Alexei1982@users.noreply.github.com>
2022-05-03 14:40:40 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.header_list_menu').click(function () {
+        $('.header_list_menu, .header_list_box').toggleClass('activ');
+    });
+});
+
+$(function () {
+    $('.header_list_menu').click(function () {
+        $('.burger_on, .burger_off').toggleClass('click');
+    });
+});
